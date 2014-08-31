f5e3a1385f4bf962a961ad271829ea174582f9da
ltijsma <l.tijsma@gmail.com>
2014-08-31 20:00:06 +0200

    Finalized

--- unit01/session1-2/preparation/06_function_fun/function_fun.js
+++ unit01/session1-2/preparation/06_function_fun/function_fun.js
@@ -11,19 +11,3 @@ var resultaat = optellen(1, 2);
 
 console.log(resultaat);
 
-/* teacher only */
-/** ANSWERS */
-
-/**
- * A) value: 3 type: Number
- * B) value: a reference to an object type: Object (with prototype Function)
- * C) value: 3 type: Number
- * D) value and type: nothing, a TypeError is thrown because resultaat
- *    is undefined and you can't call undefined
- * E) value: a reference to an object type: Object (with prototype Function)
- * F) value: a reference to an object type: Object (with prototype Function)
- *    the difference is that this object has a name now
- *    you can all resultaat.name to get optellen
- * G) value: 3 type: Number
- */
-/* end teacher only */
